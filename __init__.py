9dd419664fe8c0c29cca045300162b6d676b9484
INsiteLabDev <insitelabdev@gmail.com>
2020-04-23 13:42:36 -0700

    added option to change rate to different levels

--- __init__.py
+++ __init__.py
@@ -5,30 +5,66 @@ import mycroft.skills.mycroft_skill.audio_config as audio
 class Rate(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        self.rate_map = {"1": "'0.5'", "2": "'0.75'", "3": "'1.0'", "4": "'1.35'", "5": "'1.7'"}
+        self.rate_map = {"1": "'0.5'", "2": "'0.75'", "3": "'1.0'", "4": "'1.35'", "5": "'1.7'","one": "'0.5'", "two": "'0.75'", "three" : "'1.0'", "four": "'1.35'", "five": "'1.7'"}
 
     @intent_file_handler('fastRate.intent')
     def handle_fastRate(self, message):
-        audio.update_rate("'1.7'")
-        self.speak_dialog('fastRate')
+        rateLevel = self.rate_map["5"]
+        if (self.compare_rate(rateLevel)):
+            audio.update_rate(rateLevel)
+            self.speak_dialog('fastRate')
+        else:
+            self.speak_dialog('atCurrentRate', {'rateLevel': 'fastest'})
     
     @intent_file_handler('levelRate.intent')
     def handle_levelRate(self, message):
-        rateLevel = message.data.get('rateLevel')
-        #audio.update_rate("'1.7'")
-        audio.update_rate(self.rate_map[rateLevel])
-        self.speak_dialog('levelRate', {'rateLevel' : rateLevel})
+        rateLevel = message.data.get('ratelevel')
+        #self.speak_dialog(f"this is rate {rateLevel} and rate is {self.rate_map[rateLevel]}")
+        if rateLevel not in self.rate_map:
+            self.speak_dialog("invalidResponse")
+        else:
+            check = self.compare_rate(self.rate_map[rateLevel])
+            if check:
+                audio.update_rate(self.rate_map[rateLevel])
+                self.speak_dialog('levelRate', {'rateLevel' : rateLevel})
+            else:
+                self.speak_dialog('atCurrentRate', {'rateLevel' : rateLevel})
 
     @intent_file_handler('normalRate.intent')
     def handle_normalRate(self, message):
-        audio.update_rate("'1.0'")
-        self.speak_dialog('normalRate')
+        rateLevel = self.rate_map["3"]
+        if(self.compare_rate(rateLevel)):
+            audio.update_rate(rateLevel)
+            self.speak_dialog('normalRate')
+        else:
+            self.speak_dialog('atCurrentRate', {'rateLevel': 'normal'})
     
-    @intent_file_handler('slowRate.intent')
+    @intent_file_handler('slowestRate.intent')
+    def handle_slowestRate(self, message):
+        rateLevel = self.rate_map["1"]
+        if(self.compare_rate(rateLevel)):
+            audio.update_rate(rateLevel)
+            self.speak_dialog('slowestRate')
+        else:
+            self.speak_dialog('atCurrentRate', {'rateLevel': 'slowest'})
+    
+    @intent_file_handler('slow.intent')
     def handle_slowRate(self, message):
-        audio.update_rate("'0.7'")
-        self.speak_dialog('slowRate')
+        rateLevel = self.rate_map["2"]
+        if(self.compare_rate(rateLevel)):
+            audio.update_rate(rateLevel)
+            self.speak_dialog('slow')
+        else:
+            self.speak_dialog('atCurrentRate', {'rateLevel': 'slow'})
 
+    def compare_rate(self, rate) -> bool:
+        current_rate = audio.get_rate()
+        if current_rate != rate:
+            return True
+        return False
+    
+
+        
 
 def create_skill():
     return Rate()
